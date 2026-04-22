337aee16c8755f32e91f8b60c1ea90b31b67084d
Michael Brich <mike@toreda.com>
2026-04-22 01:41:23 -0700

    Test fix for error exit on jest test run end.

--- src/shared/slug/fonts/woff2.ts
+++ src/shared/slug/fonts/woff2.ts
@@ -1,6 +1,5 @@
-import woff2Decompress from 'woff2-encoder/decompress';
-
 export async function slugWoff2Decompress(woff2Bytes: Uint8Array): Promise<Uint8Array> {
+	const {default: woff2Decompress} = await import(/* webpackMode: "eager" */ 'woff2-encoder/decompress');
 	const result = await woff2Decompress(woff2Bytes);
 	if (!result || result.byteLength === 0) {
 		throw new Error('WOFF2 decompression failed');
